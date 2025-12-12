f0087988fec70557c58316a2798fdc0ffb296d4b
MaxWarren9 <maximvoinoff@gmail.com>
2025-12-12 23:29:27 +0300

    init commit

---
+++ src/test/java/com/sds/x6_product/X6ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.x6_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X6ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
